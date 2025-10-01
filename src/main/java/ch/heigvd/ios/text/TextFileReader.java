6ddad8e6de596b7d33e052a1f7322b165051009e
Maxime Regenass <maxime.regenass@hes-so.ch>
2025-10-01 15:36:00 +0200

    Function read and write

--- src/main/java/ch/heigvd/ios/text/TextFileReader.java
+++ src/main/java/ch/heigvd/ios/text/TextFileReader.java
@@ -1,15 +1,25 @@
 package ch.heigvd.ios.text;
 
-import ch.heigvd.ios.Readable;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * A class that reads text files. This implementation reads the file byte per byte. It manages the
  * file reader properly with a try-catch-finally block.
  */
-public class TextFileReader implements Readable {
+public class TextFileReader {
 
-  @Override
-  public void read(String filename) {
-    throw new UnsupportedOperationException("Please remove this exception and implement this method.");
-  }
+    public void read(String filename) {
+        try{
+            FileReader reader = new FileReader(filename);
+
+            int c;
+            while ((c = reader.read()) != -1) {
+                System.out.print((char)c);
+            }
+            reader.close();
+        }catch(IOException e){
+            System.out.println("Erreur de lecture" + e.getMessage());
+        }
+    }
 }
