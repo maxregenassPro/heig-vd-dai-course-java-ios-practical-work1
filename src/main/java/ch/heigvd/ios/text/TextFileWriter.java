735e458d8197d8b8a45f097f620af19884458ccb
Maxime Regenass <maxime.regenass@hes-so.ch>
2025-10-01 15:36:00 +0200

    Function read and write

--- src/main/java/ch/heigvd/ios/text/TextFileWriter.java
+++ src/main/java/ch/heigvd/ios/text/TextFileWriter.java
@@ -1,15 +1,19 @@
 package ch.heigvd.ios.text;
 
-import ch.heigvd.ios.Writable;
+import java.io.*;
 
 /**
  * A class that writes text files. This implementation write the file byte per byte. It manages the
  * file writer properly with a try-catch-finally block.
  */
-public class TextFileWriter implements Writable {
+public class TextFileWriter {
 
-  @Override
-  public void write(String filename, int sizeInBytes) {
-    throw new UnsupportedOperationException("Please remove this exception and implement this method.");
-  }
+    public void write(String filename, String tache) {
+        try (FileWriter writer = new FileWriter(filename, true))
+        {
+            writer.write("\n" + tache);
+        }catch (IOException e){
+            System.out.println("Erreur d'écriture" + e.getMessage());
+        }
+    }
 }
