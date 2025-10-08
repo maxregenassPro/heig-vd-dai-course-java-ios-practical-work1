176b597b749c7cb2ec379c81419880fd8edba0e7
Santiago Sugrañes <santiago.sugranes@heig-vd.ch>
2025-10-08 14:41:05 +0200

    finalized add command

--- src/main/java/ch/heigvd/commands/Add.java
+++ src/main/java/ch/heigvd/commands/Add.java
@@ -1,8 +1,10 @@
 package ch.heigvd.commands;
 
 import java.time.LocalDate;
-import java.util.Optional;
 import java.util.concurrent.Callable;
+
+import ch.heigvd.models.Task;
+import ch.heigvd.services.TaskService;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "add", description = "add a task")
@@ -12,36 +14,28 @@ public class Add implements Callable<Integer> {
 	@CommandLine.Parameters(index = "0", description = "The description of the task")
 	private String description;
 
-	private enum AvailablePriorityTags {
-		LOW,
-		MEDIUM,
-		HIGH
-	}
-
 	@CommandLine.Option(
 		names = {"-d", "--due"},
 		description = "Set a due date for the task (e.g. 2025-01-31)",
 		required = false)
-	private Optional<LocalDate> dueDate;
+	private LocalDate dueDate;
 
 	@CommandLine.Option(
 		names = {"-p", "--priority"},
 		description = "Set a priority to the task (possible values: ${COMPLETION-CANDIDATES}).",
 		required = false)
-	private Optional<AvailablePriorityTags> priority;
+	private Task.Priority priority;
 
 	@Override
 	public Integer call() {
-		// TODO(sss): create a task
-		// TODO(sss): implement with file IO once ready
-		System.out.println(
-			"Adding task:\n"
-			+ "global: " + (parent.getGlobalFlag() ? "true\n" : "false\n")
-			+ "description: " + description + "\n"
-			+ "priority: " + (!priority.isEmpty() ? priority.get().toString() : "NULL") + "\n"
-			+ "due: " + (!dueDate.isEmpty() ? dueDate.get().toString() : "NULL")
-		);
+		Task task = new Task();
+        task.setDescription(description.trim());
+        task.setDueDate(dueDate);
+        task.setPriority(priority);
 
+        TaskService service = new TaskService(parent.getGlobalFlag());
+        service.addTask(task);
 		return 0;
 	}
 }
+
