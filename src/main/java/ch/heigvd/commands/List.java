35efb5ab3eb5ba39284d5df63425f496e88d3778
Santiago Sugrañes <santiago.sugranes@heig-vd.ch>
2025-10-08 15:41:21 +0200

    list command: added filters and sorting

--- src/main/java/ch/heigvd/commands/List.java
+++ src/main/java/ch/heigvd/commands/List.java
@@ -4,17 +4,57 @@ import java.util.concurrent.Callable;
 
 import ch.heigvd.models.Task;
 import ch.heigvd.services.TaskService;
+import ch.heigvd.services.TaskService.Filter;
+import ch.heigvd.services.TaskService.SorterDirection;
+import ch.heigvd.services.TaskService.SorterType;
 import picocli.CommandLine;
 
 @CommandLine.Command(name = "list", description = "List all tasks.")
 public class List implements Callable<Integer> {
 	@CommandLine.ParentCommand protected Root parent;
 
+	@CommandLine.Option(
+		names = {"-f", "--filter"},
+		description = "Filter tasks (possible values are: ${COMPLETION-CANDIDATES})",
+		required = false
+	)
+	private Filter filter;
+
+	@CommandLine.Option(
+		names = {"-v", "--filter-value"},
+		description = "Value to filter by (required when using --filter)",
+		required = false
+	)
+	private String filterValue;
+
+	@CommandLine.Option(
+		names = {"-s", "--sorter"},
+		description = "Naturally sor the tasks (possible values are ${COMPLETION-CANDIDATES}).",
+		defaultValue = "ID",
+		required = false
+	)
+	private SorterType sorter;
+
+	@CommandLine.Option(
+		names = { "-d", "--direction" },
+		description = "Direction to sort tasks on (possible values: ${COMPLETION-CANDIDATES}).",
+		defaultValue = "ASC",
+		required = false)
+	private SorterDirection sortDir;
+
 	@Override
 	public Integer call() {
-		TaskService ts = new TaskService(parent.getGlobalFlag());
-		java.util.List<Task> tasks = ts.getTasks();
+		if (filter != null && (filterValue == null || filterValue.trim().isEmpty())) {
+			System.err.println("Error: --filter-value is required when using --filter");
+			return 1;
+		}
+		if (filter == null && filterValue != null && !filterValue.trim().isEmpty()) {
+			System.err.println("Error: --filter-value can only be used with --filter");
+			return 1;
+		}
 
+		TaskService ts = new TaskService(parent.getGlobalFlag());
+		java.util.List<Task> tasks = ts.getTasks(sorter, sortDir, filter, filterValue);
 		System.out.print(ts.formattedTasksString(tasks));
 
 		return 0;
