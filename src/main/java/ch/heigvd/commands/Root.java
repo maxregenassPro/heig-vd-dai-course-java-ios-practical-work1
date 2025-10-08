abd1f9548bae41ab148c8765e420d04eb893e581
Santiago Sugrañes <santiago.sugranes@heig-vd.ch>
2025-10-08 14:25:15 +0200

    root command created

--- src/main/java/ch/heigvd/commands/Root.java
+++ src/main/java/ch/heigvd/commands/Root.java
@@ -5,7 +5,20 @@ import picocli.CommandLine;
 @CommandLine.Command(
 	description = "A small TODO List CLI to experiment with Java IOs.",
 	version = "1.0.0",
+	subcommands = {
+		Add.class,
+		List.class
+	},
 	scope = CommandLine.ScopeType.INHERIT,
 	mixinStandardHelpOptions = true)
 public class Root {
+	@CommandLine.Option(
+		names = {"-g", "--global"},
+		description = "Act on the global (home directory) todo file rather than the CWD file.",
+		scope = CommandLine.ScopeType.INHERIT)
+	protected boolean globalFlag;
+
+	public boolean getGlobalFlag() {
+		return globalFlag;
+	}
 }
