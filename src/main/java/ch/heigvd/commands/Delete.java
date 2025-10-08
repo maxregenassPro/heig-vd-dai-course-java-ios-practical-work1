6e7f77f3762c19db84bf6412f2f25d9010a397d2
Maxime Regenass <maxime.regenass@hes-so.ch>
2025-10-08 14:25:01 +0200

    Add command delete

---
+++ src/main/java/ch/heigvd/commands/Delete.java
@@ -0,0 +1,25 @@
+package ch.heigvd.commands;
+
+import java.util.concurrent.Callable;
+import ch.heigvd.services.TaskService;
+import picocli.CommandLine;
+
+@CommandLine.Command(name = "delete", description = "delete task")
+public class Delete implements Callable<Integer> {
+    @CommandLine.ParentCommand protected Root parent;
+
+    @CommandLine.Option(
+            names = {"-id"},
+            description = "id of task to delete",
+            required = true)
+    protected int id;
+
+    @Override
+    public Integer call() {
+        TaskService service = new TaskService(parent.getGlobalFlag());
+        service.deleteTask(id);
+        //TODO check si tout a bien fonctionné
+
+        return 0;
+    }
+}
